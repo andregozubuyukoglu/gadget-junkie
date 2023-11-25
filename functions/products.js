5ea50663479fdd5f357355122e7ad5ed4e1c9577
Andre <andregozubuyuk@gmail.com>
2023-11-25 21:24:09 +0300

    Updated files

--- functions/products.js
+++ functions/products.js
@@ -10,6 +10,51 @@ exports.handler = async function () {
   try {
     const response = await airtable.list({ maxRecords: 200 })
 
+    exports.handler = async function () {
+      try {
+        const response = await airtable.list({ maxRecords: 200 })
+
+        const products = response.records.map((product) => {
+          const { id, fields } = product
+          const {
+            name,
+            featured,
+            price,
+            colors,
+            company,
+            description,
+            category,
+            shipping,
+            images,
+          } = fields
+          const { url } = images[0]
+          return {
+            id,
+            name,
+            featured,
+            price,
+            colors,
+            company,
+            description,
+            category,
+            shipping,
+            image: url,
+          }
+        })
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify(products),
+        }
+      } catch (error) {
+        console.log(error)
+        return {
+          statusCode: 500,
+          body: "There was an error",
+        }
+      }
+    }
+
     console.log("#######")
     console.log(response)
     console.log("#######")
--- functions/single-product.js
+++ functions/single-product.js
@@ -1,6 +1,21 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+const Airtable = require("airtable-node")
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE)
+  .table(process.env.AIRTABLE_TABLE)
+
 exports.handler = async (event, context, cb) => {
+  const { id } = event.queryStringParameters
+  if (id) {
+    return {
+      statusCode: 200,
+      body: "single products route",
+    }
+  }
   return {
-    statusCode: 200,
+    statusCode: 400,
     body: "single products route",
   }
 }
